c311aaff8a16c2b0ff3f998210308b20c18b6fa1
forestdbin <forestdb@hotmail.com>
2024-03-21 22:35:40 +0800

    Add static library

---
+++ Makefile_static.mk
@@ -0,0 +1,17 @@
+executable_hello = hello
+executable_hello_objects = main.o
+executable_hello_clean = $(executable_hello) $(executable_hello_objects)
+
+static_library_greet = greet
+static_library_greet_a = lib$(static_library_greet).a
+static_library_greet_objects = greet.o
+static_library_greet_clean = $(static_library_greet_a) $(static_library_greet_objects)
+
+$(executable_hello) : $(executable_hello_objects) $(static_library_greet_a)
+	g++ -o $@ $(executable_hello_objects) -L . -l $(static_library_greet)
+
+$(static_library_greet_a) : $(static_library_greet_objects)
+	ar crs $@ $(static_library_greet_objects)
+
+clean :
+	-@ rm -rf $(executable_hello_clean) $(static_library_greet_clean)
