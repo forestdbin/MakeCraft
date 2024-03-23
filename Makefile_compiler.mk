afb93b89e13c9e04ea13b0fef2c89f6f33e82340
forestdbin <forestdb@hotmail.com>
2024-03-24 00:45:37 +0800

    Add cross compiler

--- Makefile_compiler.mk
+++ Makefile_compiler.mk
@@ -10,4 +10,12 @@ else ifeq ($(COMPILER),CLANG)
 CC = clang
 CXX = clang++
 
+else ifeq ($(COMPILER),AARCH64_LINUX_GNU_GCC)
+
+# TOOL_PATH = /usr/bin/
+TOOL_PREFIX = aarch64-linux-gnu-
+
+CC = $(TOOL_PREFIX)gcc
+CXX = $(TOOL_PREFIX)g++
+
 endif
